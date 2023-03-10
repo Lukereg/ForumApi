0a35a827671cfe4ffcad1013f0557d417c7dbf88
Łukasz Krzysztoń <79262330+Lukereg@users.noreply.github.com>
2023-03-10 22:03:12 +0100

    Added GetAllCategories_ForAllUsers_ReturnsOkResult integration test

---
+++ ForumApi.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using FluentAssertions;
+using ForumApi.Entities;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumApi.IntegrationTests.Controllers
+{
+    public class CategoryControllerTests :IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoryControllerTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAllCategories_ForAllUsers_ReturnsOkResult()
+        {
+            //act 
+            var response = await _httpClient.GetAsync("/v1/categories");
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+    }
+}
