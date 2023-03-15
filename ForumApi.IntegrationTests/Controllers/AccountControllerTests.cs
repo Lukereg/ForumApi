8e58e0c1ffe7e09e47826752c4da3d88718d9ae8
Łukasz Krzysztoń <79262330+Lukereg@users.noreply.github.com>
2023-03-15 22:17:30 +0100

    Added RegisterUser_ForValidModel_ReturnsOk integration test

---
+++ ForumApi.IntegrationTests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using ForumApi.Entities;
+using ForumApi.IntegrationTests.Helpers;
+using ForumApi.Models.Accounts;
+using System.Runtime.Intrinsics;
+
+namespace ForumApi.IntegrationTests.Controllers
+{
+    public class AccountControllerTests : IClassFixture<CustomWebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+        private readonly CustomWebApplicationFactory<Program> _factory;
+
+        public AccountControllerTests(CustomWebApplicationFactory<Program> factory) 
+        {
+            _client = factory.CreateClient();
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task RegisterUser_ForValidModel_ReturnsOk()
+        {
+            //arrange
+            var registerUser = new RegisterUserDto()
+            {
+                Name = "Test",
+                Surname = "Testtest",
+                Email = "testemail@testemail.com",
+                Password = "password",
+                ConfirmPassword = "password",
+                Login = "testlogin"
+            };
+
+            var role = new Role() 
+            {
+                Name = "User"
+            };
+
+            var httpContent = registerUser.ToJsonHttpContent();
+
+            //seed
+            await SeedRole(role);
+
+            //act
+            var response = await _client.PostAsync("/v1/accounts/register", httpContent);
+
+            //assert
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+        }
+
+        private async Task SeedRole(Role role)
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+            var _forumDbContext = scope.ServiceProvider.GetService<ForumDbContext>();
+
+            await _forumDbContext.Roles.AddAsync(role);
+            await _forumDbContext.SaveChangesAsync();
+        }
+    }
+}
